72bc01d76030da4371ea59544d12b158d85cc29b
Divya Nain <sombeerjatt@gmail.com>
2022-03-02 18:54:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+
+
+@app.route('/bfhl', methods=['POST'])
+def bajaj_api():
+    data=request.json
+    data=data['data']
+    alp=[]
+    num=[]
+    n="0123456789"
+    for i in data:
+        if i[0] in n:
+            num.append(i)
+        else:
+            alp.append(i)
+
+    result={"is_success":True,"user_id":"girish_kumar_sharma_22022001","email":"girishsh640@gmail.com","roll_number":"11909037","numbers":num,"alphabets":alp}
+
+    return jsonify(result)
+
+app.run(host="0.0.0.0", port=8501)
